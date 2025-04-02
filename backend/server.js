d8e8b95cf32b368c28aedac2db41b2a8bc6dc694
prateek959 <prateekkush8081@gmail.com>
2025-04-02 22:13:55 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //     credentials: true
 // };
 app.use(cors({
-    origin:["http://127.0.0.1:5500"],
+    origin:["http://127.0.0.1:5500","https://webledger.netlify.app"],
     credentials: true
 }));
 
